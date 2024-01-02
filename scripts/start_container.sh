096f8c243350e05a9ebebf43d017676f07d0355e
Yashu Agrawal <AGRYASHU23@GMAIL.COM>
2024-01-02 15:24:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull agryashu23/simple-python-flas-app
+docker pull agryashu23/simple-python-flas-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 docker pull agryashu23/simple-python-flas-app
